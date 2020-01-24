8775d1f9311ee13ed067dabafed7ed4710c2a210
edvinf <edvin.fuglebakk@gmail.com>
2020-01-24 13:04:37 +0100

    minor

--- usageExamples.R
+++ usageExamples.R
@@ -4,8 +4,9 @@ plotResults <- function(estimates, title=""){
   caa <- estimates$catchAtAge / 1e6
   se <- sqrt(diag(estimates$covariance)) / 1e6
   ci <- se*1.96
-  plot(names(caa), caa, xlab="Age", ylab="catch at age (millions)", main=title, ylim=c(0,max(caa+ci)))
+  plot(1:length(caa), caa, xlab="Age", ylab="catch at age (millions)", main=title, ylim=c(0,max(caa+ci)), xaxt="n")
   segments(1:length(caa), caa+ci, y1 = caa-ci)
+  axis(side=1, 1:length(caa), names(caa))
 }
 
 data(NSSH2019)
