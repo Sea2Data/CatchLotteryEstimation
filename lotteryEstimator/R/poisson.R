45cf6af4941774239667435edfbffe5fb7448e26
edvinf <edvin.fuglebakk@gmail.com>
2020-08-28 12:59:00 +0200

    Implemented suggestion for sample loss inclusino probability calc

--- lotteryEstimator/R/poisson.R
+++ lotteryEstimator/R/poisson.R
@@ -55,3 +55,62 @@ poissonJointInclusionProbabilityMatrix <- function(inclusionProbabilities){
   diag(coinc) <- NA
   return(coinc)
 }
+
+#' Possion sampling
+#' @description
+#'  Perform Poisson sampling
+#' @details
+#'  Sampling is performed on vectors. For sampling a data frame, perform sampling on indecies.
+#'
+#'  Note the definition of 'sampleSize'. In poisson sampling the sample size is not fixed and may vary from draw to draw.
+#'
+#' @param population numeric: vector representing the population to sample from
+#' @param inclusionProbabilities numeric: vector representing the inclusion probabilites of the populatin
+#' @param sampleSize numeric: expected sample size
+#' @return numeric a subset of 'population'
+#' @examples
+#'  data("longlinerPopulation")
+#'  codset <- longlinerPopulation[longlinerPopulation$speciesFAO == "COD",]
+#'
+#'  #select cod catches with inclusion probabilites proportional to their weight
+#'  codselection <- codset[poissionSample(1:nrow(codset), codset$wholeWeightKg/sum(codset$wholeWeightKg), 100)]
+#'  nrow(codselection)
+#' @export
+poissionSample <- function(population, inclusionProbabilities, sampleSize){
+
+  if (any(inclusionProbabilities>1) | any(inclusionProbabilities<0)){
+    stop("Inclusion probabilites must be in [0,1]")
+  }
+
+  rn <- runif(length(population))
+  selection <- (rn <= inclusionProbabilities*sampleSize)
+  return(population[selection])
+}
+
+#' Random sample loss correction
+#' @description
+#'  Corrects inclusion probabilities due to random loss of samples.
+#'  Apprioriate if selected sampling units are not sampled due to random inteference.
+#'  The random interference should affect different selections with equal probability
+#'  (not related to the fact that the selections may have been made with unequal probability).
+#' @param inclusionProbability numeric: vector inclusion probabilites for sampled sampling units.
+#' @param selected The number of sampling units that was selected
+#' @param sampled The number of sampling units that was actually sampled and recorded
+#' @return numeric: vector of corrected inclusion probabilites.
+#' @examples
+#'  # correct a vector of inclusion probabilities
+#'  # 4 samples selected, two missing
+#'  randomNonResponseCorrection(c(.4,.1), 4)
+#'
+#'  # correct a single inclusion probability
+#'  # 100 samples selected. 20 missing
+#'  randomNonResponseCorrection(.1, 100, 80)
+#'
+#' @export
+randomNonResponseCorrection <- function(inclusionProbabilities, selected, sampled=length(inclusionProbabilities)){
+  lost <- selected - sampled
+  ex <- 1 - (lost / selected)
+  return(1 - (1-inclusionProbabilities)**ex)
+}
+
+
--- lotteryEstimator/tests/testthat/test-poisson.R
+++ lotteryEstimator/tests/testthat/test-poisson.R
@@ -18,3 +18,17 @@ expect_true(all(abs(vecSel - c(.0005,0.0001)) < 1e-15))
 vecPairInc <- poissonJointInclusionProbabilityMatrix(c(.0005,0.0001))
 expect_equal(vecPairInc[1,2], vecPairInc[2,1])
 expect_true(all(is.na(diag(vecPairInc))))
+
+corrected <- randomNonResponseCorrection(c(.4,.1), 4)
+expect_equal(length(corrected), 2)
+expect_lt(abs(corrected[1] - (1-(1-.4)**.5)), 1e-10)
+
+corrected <- randomNonResponseCorrection(.1, 100, 80)
+expect_equal(length(corrected), 1)
+expect_lt(abs(corrected - (1-(1-.1)**.8)), 1e-10)
+
+
+sample <- poissionSample(1:1000, 1:1000/1000, 100)
+expect_gt(length(sample), 0)
+
+expect_error(poissionSample(1:1000, 1:1000, 100))
